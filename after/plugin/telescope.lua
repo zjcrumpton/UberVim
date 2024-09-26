41abfccde3430a54f940c3cd788fc8ce72c47669
zjcrumpton <Zaccary.Crumpton@charter.com>
2024-09-26 16:35:11 -0600

    remove git focused search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,3 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fF', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Telescope find files that are in Git' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Telescope live grep' })
