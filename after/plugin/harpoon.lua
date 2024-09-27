334a1d4bd190653080addc8db63bac15daaba571
zjcrumpton <Zaccary.Crumpton@charter.com>
2024-09-26 21:35:05 -0600

    harpoon hh keymap

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,5 +3,5 @@ local harpoon = require("harpoon")
 harpoon:setup()
 
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "pin file with harpoon" })
-vim.keymap.set("n", "<leader>ho", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { noremap = true, desc = "open harpoon menu" })
+vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { noremap = true, desc = "open harpoon menu" })
 
